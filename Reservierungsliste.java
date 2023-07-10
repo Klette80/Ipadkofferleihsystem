74cf7b4c9eafb994919402cc4082b4a1a70fd209
libsch <87482793+libsch@users.noreply.github.com>
2023-07-10 15:38:01 +0200

    lida

--- Reservierungsliste.java
+++ Reservierungsliste.java
@@ -2,9 +2,16 @@ import java.util.Date;
 
 public class Reservierungsliste {
     private Knoten root;
+    private Koffer[] kofferliste;
+    private int anzahlKoffer;
+
     public Reservierungsliste()
     {
         root = new Endknoten();
+        Koffer koffer = new Koffer(1);
+        kofferliste = new Koffer[100];
+        kofferliste[1] = koffer;
+        anzahlKoffer = 1;
     }
 
     public void reservieren(Date datum, String name, Koffer koffer) {
@@ -30,5 +37,36 @@ public class Reservierungsliste {
 
     public boolean istReserviert(Date datum){
         return false;
+    }
+
+    //Koffer hinzufügen
+    public void neuerKoffer(int nummer){
+        if (kofferliste[nummer] != null){
+            System.out.println("Diese Koffernummer ist bereits vergeben");
+        } else {
+            Koffer koffer = new Koffer(nummer);
+            kofferliste[nummer] = koffer;
+            System.out.println("Der Koffer mit der Nummer " + nummer + " wurde angelegt.");
+        }
+    }
+
+    //alle angelegenten Koffer anzeigen
+    public void kofferAnzeigen(){
+        System.out.println("Lister der zur Verfügung stehenden Koffer:");
+        for(int i = 1; i < kofferliste.length; i++){
+            if (kofferliste[i] != null){
+                System.out.println("Koffernummer " + i);
+            }
+        }
+    }
+
+    //Koffer entfernen
+    public void kofferEntfernen(int nummer){
+        if(kofferliste[nummer] == null){
+            System.out.println("Der Koffer mit der Nummer " + nummer + " existiert nicht.");
+        } else {
+            kofferliste[nummer] = null;
+            System.out.println("Der Koffer mit der Nummer " + nummer + " wurde entfernt.");
+        }
     }
 }
