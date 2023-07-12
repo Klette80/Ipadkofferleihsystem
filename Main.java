39edede2c79c532dc3fe474a1f4b8f0135efd6e3
libsch <87482793+libsch@users.noreply.github.com>
2023-07-12 10:28:09 +0200

    Test 1 - 4 erstellt und getestet

--- Main.java
+++ Main.java
@@ -9,34 +9,34 @@ public class Main {
         //Test geht hier los
 
         Date testdatum = new java.util.Date();
-        Date datum1 = new Date(2023, 07, 04);
         Koffer koffer = new Koffer(0);
 
-        System.out.println("Test 1: Initialisieren einer Reservierungsliste");
-        // Initialisieren einer Reservierungsliste
-        // Reservierungsliste rs = new Reservierungsliste();
-        System.out.println("Test 2: Durchführen einer Reservierung mit den Daten '2023-11-11, Max Mustermann, Koffer 0'");
-
+        System.out.println("Test 1: Durchführen einer Reservierung mit den Daten '2023-11-11, Max Mustermann, Koffer 0'");
         // Methode reservieren ausführen.
         reservierungsliste.reservieren(testdatum, "Max Mustermann", koffer);
-        System.out.println("Test 3: Überprüfen, ob Reservierung mit den Daten '2023-11-11, Max Mustermann, Koffer 0' vorliegt.");
+        System.out.println("");
 
+        System.out.println("Test 2: Überprüfen, ob Reservierung mit den Daten '2023-11-11, Max Mustermann, Koffer 0' vorliegt.");
         // Methode istReserviert ausführen
         if (reservierungsliste.istReserviert(testdatum)) {
             System.out.println("Test erfolgreich");
         } else {
             System.out.println("Test fehlgeschlagen.");
         }
+        System.out.println("");
 
         // Methode reservieren mit dem gleichen Datum noch einmal ausführen.
-        System.out.println("Test 4: Überprüfen, ob Reservierung mit den selben Daten aus Test 3 möglich ist.");
+        System.out.println("Test 3: Überprüfen, ob Reservierung mit den selben Daten aus Test 2 möglich ist.");
         System.out.println("Erwartete Meldung: An diesem Datum liegt schon eine Reservierung vor.");
+        System.out.println("Ergebnis:");
         reservierungsliste.reservieren(testdatum, "Max Mustermann", koffer);
+        System.out.println("");
 
         // Methode stornieren ausführen.
-        reservierungsliste.stornieren(testdatum, koffer);
-        System.out.println("Test 5: Überprüfen, ob die Reservierung storniert werden kann.");
+        System.out.println("Test 4: Überprüfen, ob die Reservierung storniert werden kann.");
         System.out.println("Erwartete Meldung: Stornierung erfolgreich.");
+        System.out.println("Ergebnis:");
+        reservierungsliste.stornieren(testdatum, koffer);
 
         // Methode istReserviert ausführen
         if (reservierungsliste.istReserviert(testdatum)) {
