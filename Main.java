95ed7e9f89b1e57d8d50c26a0d6bd9e04326e87d
Klette80 <shofmann@kletterdorf.de>
2023-07-12 09:59:53 +0200

    Testszenario

--- Main.java
+++ Main.java
@@ -1,6 +1,26 @@
+import java.util.Date;
 public class Main {
 
 
     public static void main(String[] args) {}
-        Reservierungsliste reservierungsliste = new Reservierungsliste();
+    Reservierungsliste reservierungsliste = new Reservierungsliste();
+
+    Date testdatum;
+    testdatum = new java.util.Date();
+    Koffer koffer = new Koffer(0);
+    System.out.println("Test 1: Initialisieren einer Reservierungsliste");
+    // Initialisieren einer Reservierungsliste
+    Reservierungsliste rs = new Reservierungsliste();
+    System.out.println("Test 2: Durchführen einer Reservierung mit den Daten '2023-11-11, Max Mustermann, Koffer 0'");
+    // Methode reservieren ausführen.
+    rs.reservieren(testdatum, koffer, "Max Mustermann");
+    System.out.println("Test 3: Überprüfen, ob Reservierung mit den Daten '2023-11-11, Max Mustermann, Koffer 0' vorliegt.");
+    // Methode istReserviert ausführen
+        if (rs.istReserviert(testdatum)) {
+        System.out.println("Test erfolgreich");
+    }
+        else {
+        System.out.println("Test fehlgeschlagen.");
+    }
+
    }
