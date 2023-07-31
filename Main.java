5ee5d9931f6b9afec014eec6413a1dc59b07c37d
Klette80 <shofmann@kletterdorf.de>
2023-07-31 11:34:53 +0200

    Anpassung der try/catch-Logik: Die Console sollte nun keine Fehlermeldung mehr ausgeben, falls die Versionsnr. von GespeicherteListe.ser nicht identisch ist.

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.io.InvalidClassException;
 import java.util.*;
 import java.nio.file.*;
 
@@ -15,22 +16,23 @@ public class Main {
     public boolean reserviert;
 
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        reservierungsliste = new Reservierungsliste();
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InvalidClassException {
+
         ks = new KompositumSerializer();
 
-        // Der relative Dateipfad zu unserer vorhandenen Liste wird gespeichert.
-        Path dateipfad = Paths.get("GespeicherteListe.ser");
-
-        // Falls bereits eine Liste vorliegt, wird diese in die neu instanzierte Reservierungsliste geladen,
-        // damit in der Vergangenheit getätigte Reservierungen verfügbar sind.
-        if (Files.exists(dateipfad)) {
+    try {
+        // Versuche die Reservierungsliste aus der Datei GespeicherteListe.ser zu laden.
             reservierungsliste = ks.laden();
-        }
+    }
+    catch (Exception ice) {
+        // Sollte das Laden fehlschlagen, erstelle eine neues Objekt Reservierungsliste:
+        reservierungsliste = new Reservierungsliste();
+    }
 
         Testklasse test = new Testklasse();
         reservierungsliste.alleReservierungenAusgeben();
 
+        // Lade die GUI
         GUI gui = new GUI(reservierungsliste);
 
 
